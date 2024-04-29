8fe77a8a195d83eabd38a78afb3e2fdb2d346671
Sydney Spaulding <cozysydmedia@gmail.com>
2024-04-29 12:08:22 -0700

    created new counter component

--- counterwname/src/App.jsx
+++ counterwname/src/App.jsx
@@ -1,5 +1,22 @@
+import { useState } from "react";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  const [name, setName] = useState("World");
+  const [age, setAge] = useState(0);
+
+  return (
+    <div>
+      <h1>Hello, {name}!</h1>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <h2>Age: {age}</h2>
+      <button onClick={() => setAge((prevAge) => prevAge + 1)}>+</button>
+      <button onClick={() => setAge((prevAge) => prevAge - 1)}>-</button>
+    </div>
+  );
 }
 
 export default App;
